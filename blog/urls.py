a45d57aafde0e65276b858b04e7882c6233838aa
Juan Pablo <informatica@gmail.com>
2019-04-27 18:37:47 -0400

    update models, templates and html

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/<pk>/remove/', views.post_remove, name='post_remove'),
+    path('post/<pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<pk>/remove/$', views.comment_remove, name='comment_remove'),
 ]
